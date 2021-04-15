160a14a7a578b0778b1cc852a5d7dd16ad7eadb8
nathan <faustinenathaniel@gmail.com>
2021-04-15 14:32:55 +0700

    input implementation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-// import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
+
+  
 }
 
 export default App;
