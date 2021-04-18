0f1f4c5923a9b75c5c3b7051f42689f749cc27f0
nathan <faustinenathaniel@gmail.com>
2021-04-18 22:45:39 +0700

    deploy 2 model done

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ class App extends Component {
       input: '',
       wordDic: [],
       wordDicLabel: [],
-      akhiran: ''
+      akhiran: '',
+      pantunDic: [],
+      output: '',
+      reverseOutput: ''
     }
   }
 
@@ -38,39 +41,75 @@ class App extends Component {
     )
     .then(response => response.json())
     .then(users => this.setState({wordDicLabel: users}))
+
+    fetch('./generator_dict.json',{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+        }
+    }
+    )
+    .then(response => response.json())
+    .then(users => this.setState({pantunDic: users}))
   }
 
   onInputChange = (event) => {
-    this.setState({ input: event.target.value })
+    const masuk = event.target.value
+    const masukk = masuk.split(' ')
+    this.setState({ input: masuk })
+    this.setState({ output: masukk[masukk.length - 1] })
+    this.setState({ reverseOutput: masukk[masukk.length - 1] })
   }
 
   onButtonSubmit = () => {
-    const theString = this.state.input.split(" ");
-    const lastWord = theString[theString.length - 1].toLowerCase();
-    const result = lastWord.split('')
+    // const theString = this.state.input.split(" ");
+    // const lastWord = theString[theString.length - 1].toLowerCase();
+    // const result = lastWord.split('')
+    // var i;
+    // var resultt = []
+    // for (i = 0; i < result.length; i++) {
+    //   resultt.push(this.state.wordDic[result[i]]);
+    // }
+    // for (i = 0; i < 120 - result.length; i++){
+    //   resultt.unshift(0)
+    // }
+    // const converted = tf.tensor1d(resultt).expandDims(0);
+    // this.predictAkhiran(converted)
+
     var i;
-    var resultt = []
-    for (i = 0; i < result.length; i++) {
-      resultt.push(this.state.wordDic[result[i]]);
+    const forLoop = async _ => {
+      for(i = 0; i < 3; i++){
+        await this.generatePantun(this.state.output);
+        await new Promise(r => setTimeout(r, 500));
+      }
     }
-    for (i = 0; i < 120 - result.length; i++){
-      resultt.unshift(0)
-    }
-    const converted = tf.tensor1d(resultt).expandDims(0);
-    this.predictAkhiran(converted)
+    forLoop();
+    
+
+    
+    
 
 
     
 
   }
 
-  // async generatePantun(message){
-  //   console.log("started");
-  //   const model = await tf.loadLayersModel('./detectModel/model.json');
-  //   console.log(message)
-  //   // const predicted = model.predict(message)
-  //   // console.log(predicted);
-  // }
+  async generatePantun(message){
+    console.log("cooking pantun");
+    const model = await tf.loadLayersModel('./pantunModel/model.json');
+    const pantunInput = await this.preprocessPantunInput(message);
+    const predicted = model.predict(pantunInput)
+    predicted.data().then(data => {
+      const number = Math.max.apply(Math, data);
+      const index = data.indexOf(number);
+      const new_json = this.swap(this.state.pantunDic);
+      const newString = this.state.output + " " + new_json[index];
+      const reversedNewString = new_json[index] + " " + this.state.reverseOutput;
+      this.setState({output: newString});
+      this.setState({reverseOutput: reversedNewString});
+      console.log(newString);
+    })
+  }
 
   async predictAkhiran(message){
     console.log("detecting akhiran...");
@@ -81,8 +120,28 @@ class App extends Component {
       const index = data.indexOf(number)
       const new_json = this.swap(this.state.wordDicLabel)
       this.setState({akhiran: new_json[index]})
-      console.log(this.state.akhiran)
     })
+  }
+
+  preprocessPantunInput(message){
+    const newMes = message.toLowerCase();
+    const theString = newMes.split(" ");
+    var sequence = [];
+    var i;
+  
+    for (i = 0; i < theString.length; i++) {
+      var tokenizedMes = 0
+      if (theString[i] in this.state.pantunDic){
+        tokenizedMes = this.state.pantunDic[theString[i]]
+      }
+      sequence.push(tokenizedMes);
+    }
+    for (i = 0; i < 7 - theString.length; i++) {
+      sequence.unshift(0);
+    }
+    const seqConverted = tf.tensor1d(sequence).expandDims(0);
+
+    return seqConverted;
   }
 
   swap(json){
@@ -100,7 +159,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <InputForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-        <Result sampiran={this.state.input}/>
+        <Result sampiran={this.state.reverseOutput}/>
         {/* {this.loadModel()} */}
       </div>
     );
