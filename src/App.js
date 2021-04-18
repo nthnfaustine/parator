636ba13154e17d678defddc0a2e10fae5da0d41d
nathan <faustinenathaniel@gmail.com>
2021-04-18 15:46:44 +0700

    rhyme detection deployed

--- src/App.js
+++ src/App.js
@@ -12,33 +12,85 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      wordDic: [],
+      wordDicLabel: [],
+      akhiran: ''
     }
   }
 
+  componentDidMount(){
+    fetch('./word_dict.json',{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+        }
+    }
+    )
+    .then(response => response.json())
+    .then(users => this.setState({wordDic: users}))
+
+    fetch('./word_dict_label.json',{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+        }
+    }
+    )
+    .then(response => response.json())
+    .then(users => this.setState({wordDicLabel: users}))
+  }
+
   onInputChange = (event) => {
-    // console.log(event.target.value);
     this.setState({ input: event.target.value })
   }
 
   onButtonSubmit = () => {
     const theString = this.state.input.split(" ");
-    const lastWord = theString[theString.length - 1];
+    const lastWord = theString[theString.length - 1].toLowerCase();
+    const result = lastWord.split('')
+    var i;
+    var resultt = []
+    for (i = 0; i < result.length; i++) {
+      resultt.push(this.state.wordDic[result[i]]);
+    }
+    for (i = 0; i < 120 - result.length; i++){
+      resultt.unshift(0)
+    }
+    const converted = tf.tensor1d(resultt).expandDims(0);
+    this.predictAkhiran(converted)
 
+
+    
 
   }
 
-  async generatePantun(message){
-    console.log("started");
-    const model = await tf.loadLayersModel('./detectModel/model.json');
-    // model.predict()
-    // console.log(model);
-  }
+  // async generatePantun(message){
+  //   console.log("started");
+  //   const model = await tf.loadLayersModel('./detectModel/model.json');
+  //   console.log(message)
+  //   // const predicted = model.predict(message)
+  //   // console.log(predicted);
+  // }
 
   async predictAkhiran(message){
-    console.log("started");
-    const model = await tf.loadLayersModel('./detectModel/model.json');
-    // model.predict()
-    // console.log(model);
+    console.log("detecting akhiran...");
+    const model = await tf.loadLayersModel('./rhymeModel/model.json');
+    const predicted = model.predict(message)
+    predicted.data().then(data => {
+      const number = Math.max.apply(Math, data);
+      const index = data.indexOf(number)
+      const new_json = this.swap(this.state.wordDicLabel)
+      this.setState({akhiran: new_json[index]})
+      console.log(this.state.akhiran)
+    })
+  }
+
+  swap(json){
+    var ret = {};
+    for(var key in json){
+      ret[json[key]] = key;
+    }
+    return ret;
   }
 
   render(){
